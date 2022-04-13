416136f5002becfa391e79ad8fd5ca36484622ab
Phoenix5050 <59894902+Phoenix5050@users.noreply.github.com>
2022-04-13 14:42:01 -0400

    Uploading Completed project

---
+++ hyrule-jobs/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import JobsList from './components/JobsList.vue'
+import Job from './types/Job'
+import OrderTerm from './types/OrderTerm'
+
+// multi data type for use with variables
+// let age: number | number = 25
+
+export default defineComponent({
+  name: 'App',
+  components: { JobsList },
+  setup() {
+    // reactive function implementation
+    // const state = reactive({
+    //   name: 'Link',
+    //   // inside data object, use type assertion with 'as' keyword
+    //   age: 25 as number | string
+    // })
+
+    // state.name = 'Sean'
+    // state.age = 26
+
+    // return { ...toRefs(state)}
+
+    // refs implementation
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+
+    // return { name, age }
+
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2'},
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3'},
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'},
+    ])
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
+  }
+});
+</script>
+
+<style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
+  }
+</style>
